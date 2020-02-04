ae928070895b9cedceed3f61d87f4c6cc3f3dfab
Rubén Hernández Vicente <r.hernandez@tvt.es>
2020-02-04 14:32:36 +0100

    Fix LiPo battery voltages

--- include/battery.hpp
+++ include/battery.hpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include "hal.hpp"
 
-#define BATTERY_MIN_V 2.9
-#define BATTERY_MAX_V 4.0
+#define BATTERY_MIN_V 3.8
+#define BATTERY_MAX_V 4.1
 
 float getVoltage();
 uint8_t calcPercentage(float volts);
